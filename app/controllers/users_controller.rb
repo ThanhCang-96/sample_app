4eab5d10106bb28b1fa88685e50daea6c6129a1e
Thanh Cang <thanhcang.250396@gmail.com>
2020-12-29 08:25:27 +0700

    Chapter_9: Advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = t "users.create_user_successful"
+      log_in @user
       redirect_to @user
     else
       render :new
