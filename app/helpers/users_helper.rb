b5ca8c52fb8271dafe503dda31d506dd6694b12b
Thanh Cang <thanhcang.250396@gmail.com>
2020-12-29 08:25:27 +0700

    Account activation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,13 @@
 module UsersHelper
   def gravatar_for user, size: Settings.userhelper.gravatar_for.size
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest user.email.downcase
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def admin? user
     if current_user.admin? && !current_user?(user)
-      link_to "delete", user,
-        method: :delete,
+      link_to "delete", user, method: :delete,
         data: {confirm: t("_user.confirm")}
     end
   end
